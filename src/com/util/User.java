b14fc787fd02f5561b2ff426c9ffc8624d6fdfa8
Rajeev Kumar <VE00YM057@YMI.COM>
2019-02-23 10:22:58 +0530

    configuration +  util

---
+++ src/com/util/User.java
@@ -0,0 +1,22 @@
+package com.util;
+
+import com.bean.Credentials;
+import com.bean.Profile;
+
+public class User {
+	public String login(Credentials credentials) {
+		return "";
+	}
+
+	public boolean logout(String userId) {
+		return false;
+	}
+
+	public String changePassword(Credentials credentials, String newPassword) {
+		return "";
+	}
+
+	public String register(Profile profile) {
+		return "";
+	}
+}
